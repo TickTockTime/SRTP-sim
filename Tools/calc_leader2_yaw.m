0b4c6797f14f6622a84dc161ce26bf708e56555e
TickTockTime <3160102438@zju.edu.cn>
2019-05-09 09:23:38 +0800

    calc-leader2-yaw

---
+++ Tools/calc_leader2_yaw.m
@@ -0,0 +1,18 @@
+%% 工具函数：求解Leader2领航机偏航角函数
+% Author: Yao Chengtao
+% 2018/8/5
+
+%% 参数初始化
+syms y t;
+
+fd = 0.5;
+v1x = -0.5*sin(0.25*t);
+v1y = 0.5*cos(0.5*t);
+v1 = sqrt( v1x.^2 + v1y.^2);
+yaw1 = atan2( v1x, v1y);
+
+%% 求解不同初始偏航角的Leader2偏航角函数
+dsolve('Dy=v1/fd*sin(yaw1-y)','y(0)=pi/2')                                  % 初始偏航角：pi/2
+dsolve('Dy=v1/fd*sin(yaw1-y)','y(0)=-pi/2')                                 % 初始偏航角：-pi/2
+dsolve('Dy=v1/fd*sin(yaw1-y)','y(0)=pi')                                     % 初始偏航角：pi
+dsolve('Dy=v1/fd*sin(yaw1-y)','y(0)=-pi')                                    % 初始偏航角：-pi
